829762aab83a70c9e5832ed686f5fe02ddbf0d32
Pete Wilkins <pete.p.wilkins@gmail.com>
2017-05-03 10:44:26 +0100

    adds devise gem to manage user sign up/in/out etc.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root "restaurants#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :restaurants do
     resources :reviews
-  end    
+  end
 end
